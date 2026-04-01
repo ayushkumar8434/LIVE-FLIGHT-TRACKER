180d34568381f501c290afd8d6d8e4ededf75b67
Ayush Kumar <kumarayush17949@gmail.com>
2026-04-01 23:48:01 +0530

    Refactor fetch API call and improve error handling

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function searchFlight() {
 
   result.innerHTML = "Loading...";
 
-  fetch(`fetch("https://api.allorigins.win/raw?url=http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&flight_iata=${flightNumber}")`)
+  fetch(`https://api.allorigins.win/raw?url=http://api.aviationstack.com/v1/flights?access_key=${API_KEY}&flight_iata=${flightNumber}`)
     .then(res => res.json())
     .then(data => {
 
